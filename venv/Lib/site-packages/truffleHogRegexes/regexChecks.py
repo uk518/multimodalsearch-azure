f137610b1d77b2022023a03962f2c1bbf9dff796
abckuj <udaya.mantri123@gmail.com>
2025-09-20 12:57:37 +0530

    Add .gitignore to ignore venv

--- venv/Lib/site-packages/truffleHogRegexes/regexChecks.py
+++
@@ -1,9 +0,0 @@
-import re
-import json
-import os
-
-with open(os.path.join(os.path.dirname(__file__), "regexes.json"), 'r') as f:
-    regexes = json.loads(f.read())
-
-for key in regexes:
-    regexes[key] = re.compile(regexes[key])
